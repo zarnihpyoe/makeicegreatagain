063bb8b76ea3855ea1523aff95c1ea3bdf9c9639
Zarni Phyo <zarniphyo95@gmail.com>
2017-02-16 20:08:01 -0500

    feat: Show all books when GET /books/

--- models/book.js
+++ models/book.js
@@ -4,20 +4,31 @@ const mongoose = require('mongoose')
 // Import config
 const configDB = require('../config/database')
 
+
 // Book Schema
 const BookSchema = mongoose.Schema({
+  googleId: {type: String, required: true},
   title: {type: String, required: true},
   subtitle: String,
   authors: [String],
   publisher: {type: String, required: true},
   publishedDate: {type: String, required: true},
   description: String,
+  isbn_10: {type: String},
   isbn_13: {type: String, required: true},
   pageCount: Number,
-  catalogedDate: {type: Date, required: true},
+  catalogedDate: {type: Date, default: Date.now},
+  imageLinks: {
+    smallThumbnail: String,
+    thumbnail: String
+  }
 })
 
 const Book = module.exports = mongoose.model('Book', BookSchema)
+
+module.exports.getAll = function(filter, callback) {
+  Book.find(filter).exec(callback)
+}
 
 module.exports.addBook = function(newBook, callback) {
   newBook.save(callback)
--- routes/books.js
+++ routes/books.js
@@ -11,7 +11,10 @@ const router = express.Router()
 
 // Get all the books in the database
 router.get('/', (req, res, next) => {
-
+  Book.getAll({}, (err, books) => {
+    if(err) throw err
+    res.send(books)
+  })
 })
 
 // Get a specific book
@@ -24,18 +27,24 @@ router.post('/', (req, res, next) => {
   const url = 'https://www.googleapis.com/books/v1/volumes/' + req.body.bid + '?fields=id,volumeInfo(title,subtitle,authors,publisher,publishedDate,description,industryIdentifiers,pageCount,categories,imageLinks(smallThumbnail,thumbnail))'
   bookParser.getJSON(url).then((book) => {
     let newBook = new Book({
+      googleId: book.id,
       title: book.volumeInfo.title,
       subtitle: book.volumeInfo.subtitle,
       authors: book.volumeInfo.authors,
       publisher: book.volumeInfo.publisher,
       publishedDate: book.volumeInfo.publishedDate,
       description: book.volumeInfo.description,
+      isbn_10: book.volumeInfo.industryIdentifiers[0].identifier,
       isbn_13: book.volumeInfo.industryIdentifiers[1].identifier,
       pageCount: book.volumeInfo.pageCount,
-      catalogedDate: new Date(),
+      //catalogedDate: new Date(),
+      imageLinks: {
+        smallThumbnail: book.volumeInfo.imageLinks.smallThumbnail,
+        thumbnail: book.volumeInfo.imageLinks.thumbnail
+      }
     })
 
-    Book.addBook(newBook, (err, user) => {
+    Book.addBook(newBook, (err, book) => {
       if(err) res.json({success: false, msg: 'Failed to add the book.'})
       else res.json({success: true, msg: 'Book added to the database.'})
     })
